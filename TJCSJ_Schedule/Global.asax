d31b7c69417936cbb1f455fe637780dd7ada0119
Samuel <Samuel@KNIGHT>
2016-07-27 15:25:04 -0700

    Add project files.

---
+++ TJCSJ_Schedule/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TJCSJ_Schedule.MvcApplication" Language="C#" %>
---
+++ TJCSJ_Schedule/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TJCSJ_Schedule.Startup))]
+namespace TJCSJ_Schedule
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
